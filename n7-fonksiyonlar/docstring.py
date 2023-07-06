32b6e1576492ff19116368d12a89a5af83c4fa21
Fatime Sevilgen <fatimesevilgen@gmail.com>
2023-07-06 15:12:28 +0300

    güncelleme

---
+++ n7-fonksiyonlar/docstring.py
@@ -0,0 +1,16 @@
+def summer(arg1,arg2):
+
+    """
+    sum of two numbers     #fonsiyonun görevi
+    Args:
+        arg1: int,float    #argümanlarin tip bilgisi
+                           #argümanin görevi de girilebilir
+        arg2: int,float
+
+    Returns:               #Geri döndürülen değerin açiklamasi
+        int,float
+            
+    """
+    
+    print(arg1+arg2)
+
